bfa540a08908b158e5fa22216bc45ebb4761554a
Venom-0ff <kostukov.stepan.o@gmail.com>
2023-11-19 21:18:05 -0500

    Implemented MVVM.

--- app/src/main/java/com/skostyukov/nytfeed/MainActivity.kt
+++ app/src/main/java/com/skostyukov/nytfeed/MainActivity.kt
@@ -2,19 +2,13 @@ package com.skostyukov.nytfeed
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.google.gson.Gson
 import com.skostyukov.nytfeed.databinding.ActivityMainBinding
 import com.skostyukov.nytfeed.model.APIFormat
 import com.skostyukov.nytfeed.viewmodel.MainViewModel
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
-import kotlinx.coroutines.launch
-import java.io.InputStreamReader
-import java.net.URL
-import javax.net.ssl.HttpsURLConnection
 
 class MainActivity : AppCompatActivity() {
     private lateinit var recyclerViewManager: RecyclerView.LayoutManager
@@ -25,50 +19,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+
+        val apiObserver = Observer<APIFormat> {
+                newAPI -> updateUI(newAPI)//binding.api = newAPI
+        }
+
+        viewModel.apiFormat.observe(this, apiObserver)
 
         // setup recyclerView
         recyclerViewManager = LinearLayoutManager(applicationContext)
         binding.recyclerView.layoutManager = recyclerViewManager
         binding.recyclerView.setHasFixedSize(true)
 
-        startCoroutine()
-    }
-
-    private fun startCoroutine() {
-        CoroutineScope(Dispatchers.Main).launch {
-            val request: APIFormat? = getBookDataFromCoroutine()
-
-            if (request != null && request.results.isNotEmpty()) {
-                updateUI(request)
-            }
-        }
+        viewModel.startCoroutine()
     }
 
     private fun updateUI(request: APIFormat) {
 //        binding.textViewCopyright.text = request.copyright
         binding.recyclerView.adapter =RecyclerAdapter(request.results)
-    }
-
-    private suspend fun getBookDataFromCoroutine(): APIFormat? {
-        val defer = CoroutineScope(Dispatchers.IO).async {
-            val url = URL("https://api.nytimes.com/svc/news/v3/content/all/world.json?api-key=435dnRm9ZmRAYv7gnsTuC8tgbFCfe90n")
-            val connection = url.openConnection() as HttpsURLConnection
-
-            if (connection.responseCode == 200) {
-                val inputSystem = connection.inputStream
-                val inputStreamReader = InputStreamReader(inputSystem, "UTF-8")
-                val request = Gson().fromJson(inputStreamReader, APIFormat::class.java)
-
-                inputStreamReader.close()
-                inputSystem.close()
-
-                return@async request
-            }
-            else {
-                return@async null
-            }
-        }
-
-        return defer.await()
     }
 }
--- app/src/main/java/com/skostyukov/nytfeed/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/skostyukov/nytfeed/viewmodel/MainViewModel.kt
@@ -1,4 +1,51 @@
 package com.skostyukov.nytfeed.viewmodel
 
-class MainViewModel {
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.google.gson.Gson
+import com.skostyukov.nytfeed.model.APIFormat
+import com.skostyukov.nytfeed.model.Result
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+import java.io.InputStreamReader
+import java.net.URL
+import javax.net.ssl.HttpsURLConnection
+
+class MainViewModel: ViewModel() {
+    var apiFormat = MutableLiveData<APIFormat>(APIFormat("", listOf<Result>()))
+
+    fun startCoroutine() {
+        CoroutineScope(Dispatchers.Main).launch {
+            val request: APIFormat? = getDataFromCoroutine()
+
+            if (request != null && request.results.isNotEmpty()) {
+                apiFormat.value = request!!
+            }
+        }
+    }
+
+    private suspend fun getDataFromCoroutine(): APIFormat? {
+        val defer = CoroutineScope(Dispatchers.IO).async {
+            val url = URL("https://api.nytimes.com/svc/news/v3/content/all/world.json?api-key=435dnRm9ZmRAYv7gnsTuC8tgbFCfe90n")
+            val connection = url.openConnection() as HttpsURLConnection
+
+            if (connection.responseCode == 200) {
+                val inputSystem = connection.inputStream
+                val inputStreamReader = InputStreamReader(inputSystem, "UTF-8")
+                val request = Gson().fromJson(inputStreamReader, APIFormat::class.java)
+
+                inputStreamReader.close()
+                inputSystem.close()
+
+                return@async request
+            }
+            else {
+                return@async null
+            }
+        }
+
+        return defer.await()
+    }
 }
